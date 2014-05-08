dbcb2e402f2fe609b25a15930dad4d365e0fb613
Hack Reactor Students <rcpeng@gmail.com>
2014-05-07 21:55:19 -0700

    Updates quotation to single

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
 exports.headers = headers = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'text/html'
 };
 
 // As you progress, keep thinking about what helper functions you can put here!
