49d39b38feb4a46da40d7d2c63831364b1d046d7
Hack Reactor Students <rcpeng@gmail.com>
2014-05-07 20:27:35 -0700

    Extracts and modularizes helper functions, adds post request handling

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,50 +4,62 @@ var httpHelpers = require('./http-helpers.js');
 var fs = require('fs');
 // require more modules/folders here!
 
-var readFileHelper = function (response, location, contentType) {
-  fs.readFile(location, function(err, data) {
-    if(err) {
-      throw err;
-    }
-    response.writeHead(200, {'Content-Type': contentType});
-    response.write(data);
-    console.log('data', data.toString());
-    response.end();
-  });
-};
-
-
-
 exports.handleRequest = function (req, res) {
   if (req.method === 'GET') {
     if (req.url === '/') {
-      readFileHelper(res, '../web/public/index.html', 'text/html');
+      httpHelpers.serveAssets(res, '../web/public/index.html', 'text/html', 200);
     } else if (req.url ==='/styles.css') {
-      readFileHelper(res, '../web/public/styles.css', 'text/css');
+      httpHelpers.serveAssets(res, '../web/public/styles.css', 'text/css', 200);
     } else if (req.url ==='/loading.html') {
-      readFileHelper(res, '../web/public/loading.html', 'text/html');
-    } else {
-
-      var newUrl = req.url.slice(1);
-      fs.readFile('../archives/sites.txt', function(err, sites){
-        if(err) {
-          throw err;
-        }
-        var siteObj = JSON.parse('{' + sites + '}');
-        // if newUrl is in siteObj, get it from ./archives/sites
-        // if it isn't, download and archive
-        if (siteObj[newUrl]) {
-          readFileHelper(res, '../archives/sites/'+newUrl, 'text/html');
-        } else {
-
-
-        }
-      });
+      httpHelpers.serveAssets(res, '../web/public/loading.html', 'text/html', 200);
     }
   }
+  if(req.method === 'POST') {
+    var url = '';
+    req.on('data', function(chunk) {
+      url += chunk;
+    });
+
+    req.on('end', function(){
+      console.log(url);
+      // at this point url is a string of form 'url=*inputfield*'
+      url = url.slice(4);
+      // url is now exactly what is in the input form
+      if(archive.isUrlInList(url)) {
+        httpHelpers.serveAssets(res, '../archives/sites/' + url, 'text/html', 200);
+      } else {
+        archive.addUrlToList(url);
+        archive.downloadUrls(url, function(){
+          httpHelpers.serveAssets(res, '../archives/sites/' + url, 'text/html', 302);
+        });
+      }
+    });
+  }
+};
+
 
 
   // res.end(archive.paths.list);
 
+    //   var newUrl = req.url.slice(1);
+    //   fs.readFile('../archives/sites.txt', function(err, sites){
+    //     if(err) {
+    //       throw err;
+    //     }
+    //     var siteObj = JSON.parse('{' + sites + '}');
+    //     // if newUrl is in siteObj, get it from ./archives/sites
+    //     // if it isn't, download and arcive
+    //     if (siteObj[newUrl]) h{
+    //       httpHelpers.serveAssets(res, '../archives/sites/'+newUrl, 'text/html');
+    //     } else {
 
-};
+
+    //     }
+    //   });
+    // }
+        // } else {
+        //   siteObj[url] = true;
+        //   var input = JSON.stringify(siteObj);
+        //   fs.writeFile('../archives/sites.txt', input);
+        //   res.end();
+        // }
