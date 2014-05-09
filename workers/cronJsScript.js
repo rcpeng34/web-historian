b751b031299c392c4e1583deb01e3ccb9dbf5ef8
Hack Reactor Students <rcpeng@gmail.com>
2014-05-08 19:46:04 -0700

    Updates with downloadUrl call

--- workers/cronJsScript.js
+++ workers/cronJsScript.js
@@ -1,1 +1,7 @@
-console.log("hello!");
+// var path = require('path');
+// var httpHelpers = require('./http-helpers.js');
+// var fs = require('fs');
+// var htmlFetcher = require('../workers/htmlfetcher.js');
+var archive = require('../helpers/archive-helpers.js');
+
+archive.downloadUrls();
